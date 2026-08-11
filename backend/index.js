ecfbd6451a1f9e4fa9775b26b70bc5e4955f2ae2
M. Saim Malik <msaimmalik1010@gmail.com>
2026-08-11 13:25:52 +0500

    fix: correct API route paths in index.js and add proxy to package.json

--- backend/index.js
+++ backend/index.js
@@ -23,10 +23,10 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
-app.use("api/products", require("./routes/productRoutes"));
-app.use("api/orders", require("./routes/orderRoutes"));
-app.use("api/payment", require("./routes/paymentRoutes"));
-app.use("api/analytics", require("./routes/analyticsRoutes"));
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+app.use("/api/payment", require("./routes/paymentRoutes"));
+app.use("/api/analytics", require("./routes/analyticsRoutes"));
 const PORT = process.env.PORT || 5000;
 
 console.log("Starting server...");
